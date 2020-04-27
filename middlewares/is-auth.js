79996d5a5107cb52159bb0ddf6a971a54c477d70
Jack2ee <woogbi@likelion.org>
2020-04-27 16:02:38 +0900

    fix: encrypt&decrypt token from client

--- middlewares/is-auth.js
+++ middlewares/is-auth.js
@@ -1,27 +1,45 @@
 const jwt = require("jsonwebtoken");
+const CryptoJS = require("crypto-js");
 
 const JWT_SECRET_KEY = "Workoutlove";
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   const authHeader = req.get("Authorization");
   if (!authHeader) {
     const error = new Error("권한이 없습니다.");
     error.statusCode = 401;
     throw error;
   }
-  const authToken = authHeader.split(" ")[1];
-  let decodedToken;
+  const uglifiedToken = authHeader.split(" ")[1];
+
+  let authToken;
   try {
-    decodedToken = jwt.verify(authToken, JWT_SECRET_KEY);
+    const AESbytes = await CryptoJS.AES.decrypt(
+      uglifiedToken,
+      `${JWT_SECRET_KEY}`
+    );
+    authToken = await AESbytes.toString(CryptoJS.enc.Utf8);
   } catch (err) {
-    err.statusCode = 500;
-    throw err;
+    if (!err.statusCode) {
+      err.statusCode = "토큰 바이트화에 실패하였습니다.";
+    }
   }
-  if (!decodedToken) {
-    const error = new Error("권한이 없습니다.");
-    error.statusCode = 401;
-    throw error;
+
+  let decodedToken;
+  if (authToken) {
+    try {
+      decodedToken = await jwt.verify(authToken, JWT_SECRET_KEY);
+    } catch (err) {
+      err.statusCode = 500;
+      throw err;
+    }
+    if (!decodedToken) {
+      const error = new Error("권한이 없습니다.");
+      error.statusCode = 401;
+      throw error;
+    }
   }
+
   req.userId = decodedToken.userId;
   next();
 };
